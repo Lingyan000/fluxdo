e32e672628fed3f99a0bd202251e291794095000
dashu33 <83408280@qq.com>
2026-07-14 03:59:37 +0800

    feat: add goBack action to topic progress gestures
    Allow binding the progress pill swipe/long-press menu to a page back
    action so mobile users can leave a topic without reaching the top-left
    back button.

--- lib/pages/topic_detail_page/topic_detail_page.dart
+++ lib/pages/topic_detail_page/topic_detail_page.dart
@@ -1445,6 +1445,10 @@ class _TopicDetailPageState extends ConsumerState<TopicDetailPage>
             .enterSearchMode();
       case ProgressGestureAction.refresh:
         unawaited(_handleRefresh());
+      case ProgressGestureAction.goBack:
+        if (mounted) {
+          unawaited(Navigator.of(context).maybePop());
+        }
     }
   }
 
